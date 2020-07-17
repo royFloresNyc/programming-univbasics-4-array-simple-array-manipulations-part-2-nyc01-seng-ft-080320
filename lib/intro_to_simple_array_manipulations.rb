ee073d6f65854786c9380fd132b07a8493f14436
Roy  Flores <royfloresnyc@gmail.com>
2020-07-17 21:07:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,3 +10,14 @@ def using_uniq(array)
   array.uniq 
 end
 
+def using_flatten(array)
+  array.flatten 
+end 
+
+def using_delete(array, element)
+  array.delete(element)
+end 
+
+def using_delete_at(array, index)
+  array.delete_at(index)
+end 
